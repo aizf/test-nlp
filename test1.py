8d7cbe2cd86179b1f6482fe6a6dce7b14da46a45
aizf <>
2019-10-18 17:55:30 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -15,12 +15,12 @@ text = "[CLS] Who was Jim Henson ? [SEP] Jim Henson was a puppeteer [SEP]"
 tokenized_text = tokenizer.tokenize(text)
 
 # Mask a token that we will try to predict back with `BertForMaskedLM`
-masked_index = 8
+masked_index = 11
 tokenized_text[masked_index] = '[MASK]'
-assert tokenized_text == [
-    '[CLS]', 'who', 'was', 'jim', 'henson', '?', '[SEP]', 'jim', '[MASK]',
-    'was', 'a', 'puppet', '##eer', '[SEP]'
-]
+# assert tokenized_text == [
+#     '[CLS]', 'who', 'was', 'jim', 'henson', '?', '[SEP]', 'jim', '[MASK]',
+#     'was', 'a', 'puppet', '##eer', '[SEP]'
+# ]
 
 # Convert token to vocabulary indices
 indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
@@ -31,13 +31,36 @@ segments_ids = [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1]
 tokens_tensor = torch.tensor([indexed_tokens])
 segments_tensors = torch.tensor([segments_ids])
 
-# Part II
+# # Part II
+
+# # Load pre-trained model (weights)
+# model = BertModel.from_pretrained('bert-base-uncased')
+
+# # Set the model in evaluation mode to deactivate the DropOut modules
+# # This is IMPORTANT to have reproducible results during evaluation!
+# model.eval()
+
+# # If you have a GPU, put everything on cuda
+# tokens_tensor = tokens_tensor.to('cuda')
+# segments_tensors = segments_tensors.to('cuda')
+# model.to('cuda')
+
+# # Predict hidden states features for each layer
+# with torch.no_grad():
+#     # See the models docstrings for the detail of the inputs
+#     outputs = model(tokens_tensor, token_type_ids=segments_tensors)
+#     # Transformers models always output tuples.
+#     # See the models docstrings for the detail of all the outputs
+#     # In our case, the first element is the hidden state of the last layer of the Bert model
+#     encoded_layers = outputs[0]
+# # We have encoded our input sequence in a FloatTensor of shape (batch size, sequence length, model hidden dimension)
+# assert tuple(encoded_layers.shape) == (1, len(indexed_tokens),
+#                                        model.config.hidden_size)
+
+# part III
 
 # Load pre-trained model (weights)
-model = BertModel.from_pretrained('bert-base-uncased')
-
-# Set the model in evaluation mode to deactivate the DropOut modules
-# This is IMPORTANT to have reproducible results during evaluation!
+model = BertForMaskedLM.from_pretrained('bert-base-uncased')
 model.eval()
 
 # If you have a GPU, put everything on cuda
@@ -45,14 +68,15 @@ tokens_tensor = tokens_tensor.to('cuda')
 segments_tensors = segments_tensors.to('cuda')
 model.to('cuda')
 
-# Predict hidden states features for each layer
+# Predict all tokens
 with torch.no_grad():
-    # See the models docstrings for the detail of the inputs
     outputs = model(tokens_tensor, token_type_ids=segments_tensors)
-    # Transformers models always output tuples.
-    # See the models docstrings for the detail of all the outputs
-    # In our case, the first element is the hidden state of the last layer of the Bert model
-    encoded_layers = outputs[0]
-# We have encoded our input sequence in a FloatTensor of shape (batch size, sequence length, model hidden dimension)
-assert tuple(encoded_layers.shape) == (1, len(indexed_tokens),
-                                       model.config.hidden_size)
+    predictions = outputs[0]
+
+# confirm we were able to predict 'henson'
+predicted_index = torch.argmax(predictions[0, masked_index]).item()
+predicted_token = tokenizer.convert_ids_to_tokens([predicted_index])[0]
+# assert predicted_token == 'henson'
+print(tokens_tensor)
+print(segments_tensors)
+print(predicted_token)
