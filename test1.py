c953d89148e90fd7a950e4114efd59a1f1e03ad0
aizf <>
2019-10-22 07:20:39 +0800

    1

--- test1.py
+++ test1.py
@@ -1,7 +1,5 @@
-# Part I
-
 import torch
-from transformers import BertTokenizer, BertModel, BertForMaskedLM
+from transformers import BertTokenizer, BertModel, BertForSequenceClassification
 
 # OPTIONAL: if you want to have more information on what's happening under the hood, activate the logger as follows
 import logging
@@ -11,72 +9,28 @@ logging.basicConfig(level=logging.INFO)
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
 # Tokenize input
-text = "[CLS] Who was Jim Henson ? [SEP] Jim Henson was a puppeteer [SEP]"
-tokenized_text = tokenizer.tokenize(text)
-
-# Mask a token that we will try to predict back with `BertForMaskedLM`
-masked_index = 11
-tokenized_text[masked_index] = '[MASK]'
-# assert tokenized_text == [
-#     '[CLS]', 'who', 'was', 'jim', 'henson', '?', '[SEP]', 'jim', '[MASK]',
-#     'was', 'a', 'puppet', '##eer', '[SEP]'
-# ]
-
-# Convert token to vocabulary indices
-indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
-# Define sentence A and B indices associated to 1st and 2nd sentences (see paper)
-segments_ids = [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1]
-
+text = "Hello, my dog is cute"
+indexed_tokens = tokenizer.convert_tokens_to_ids(
+    ['[CLS]'] + tokenizer.tokenize(text) + ['[SEP]'])
 # Convert inputs to PyTorch tensors
-tokens_tensor = torch.tensor([indexed_tokens])
-segments_tensors = torch.tensor([segments_ids])
+tokens_tensor = torch.tensor(indexed_tokens).unsqueeze(0)
+# 相当于 tokens_tensor = torch.tensor([indexed_tokens])
 
-# # Part II
+model = BertForSequenceClassification.from_pretrained('bert-base-uncased')
 
-# # Load pre-trained model (weights)
-# model = BertModel.from_pretrained('bert-base-uncased')
-
-# # Set the model in evaluation mode to deactivate the DropOut modules
-# # This is IMPORTANT to have reproducible results during evaluation!
-# model.eval()
-
-# # If you have a GPU, put everything on cuda
-# tokens_tensor = tokens_tensor.to('cuda')
-# segments_tensors = segments_tensors.to('cuda')
-# model.to('cuda')
-
-# # Predict hidden states features for each layer
-# with torch.no_grad():
-#     # See the models docstrings for the detail of the inputs
-#     outputs = model(tokens_tensor, token_type_ids=segments_tensors)
-#     # Transformers models always output tuples.
-#     # See the models docstrings for the detail of all the outputs
-#     # In our case, the first element is the hidden state of the last layer of the Bert model
-#     encoded_layers = outputs[0]
-# # We have encoded our input sequence in a FloatTensor of shape (batch size, sequence length, model hidden dimension)
-# assert tuple(encoded_layers.shape) == (1, len(indexed_tokens),
-#                                        model.config.hidden_size)
-
-# part III
-
-# Load pre-trained model (weights)
-model = BertForMaskedLM.from_pretrained('bert-base-uncased')
-model.eval()
+labels = torch.tensor([1]).unsqueeze(0)  # Batch size 1
 
 # If you have a GPU, put everything on cuda
 tokens_tensor = tokens_tensor.to('cuda')
-segments_tensors = segments_tensors.to('cuda')
-model.to('cuda')
+model = model.to('cuda')
+labels = labels.to('cuda')
 
-# Predict all tokens
+#
 with torch.no_grad():
-    outputs = model(tokens_tensor, token_type_ids=segments_tensors)
-    predictions = outputs[0]
+    outputs = model(tokens_tensor, labels=labels)
+    loss, logits = outputs[:2]
 
-# confirm we were able to predict 'henson'
-predicted_index = torch.argmax(predictions[0, masked_index]).item()
-predicted_token = tokenizer.convert_ids_to_tokens([predicted_index])[0]
-# assert predicted_token == 'henson'
 print(tokens_tensor)
-print(segments_tensors)
-print(predicted_token)
+print(outputs)
+print(loss)
+print(logits)
